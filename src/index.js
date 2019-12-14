f0b12ca2f98171126a0878d8e9d92b73f9c1b79c
Doruk Kutlu <me@doruk.me>
2019-12-14 22:44:19 +0300

    export moar HTML tags

--- src/index.js
+++ src/index.js
@@ -1,4 +1,64 @@
 import "css.escape";
+import styled from "styled-components";
 
 export { default as Button } from "./Button";
 export { default as Loader } from "./Loader";
+
+export const Ul = styled.ul`
+  list-style-type: none;
+`;
+
+export const Li = styled.li`
+  padding: 0 0.3rem;
+`;
+
+export const Main = styled.main`
+  padding: 0.5rem;
+`;
+
+export const P = styled.p`
+  padding-left: 0.3rem;
+`;
+
+export const Span = styled.span``;
+export const Nav = styled.nav``;
+export const Div = styled.div``;
+export const Section = styled.section``;
+export const H1 = styled.h1``;
+export const H2 = styled.h2``;
+export const H3 = styled.h3``;
+export const H4 = styled.h4``;
+export const H5 = styled.h5``;
+export const H6 = styled.h6``;
+export const A = styled.a``;
+export const Article = styled.article``;
+export const Aside = styled.aside``;
+export const Audio = styled.audio``;
+export const B = styled.b``;
+export const Br = styled.br``;
+export const Caption = styled.caption``;
+export const Code = styled.code``;
+export const Footer = styled.footer``;
+export const Header = styled.header``;
+export const Hr = styled.hr``;
+export const I = styled.i``;
+export const Iframe = styled.iframe``;
+export const Img = styled.img``;
+export const Input = styled.input``;
+export const Ol = styled.ol``;
+export const Option = styled.option``;
+export const Picture = styled.picture``;
+export const Pre = styled.pre``;
+export const Progress = styled.progress``;
+export const Select = styled.select``;
+export const Source = styled.source``;
+export const Table = styled.table``;
+export const Tbody = styled.tbody``;
+export const Td = styled.td``;
+export const Textarea = styled.textarea``;
+export const Tfoot = styled.tfoot``;
+export const Th = styled.th``;
+export const Thead = styled.thead``;
+export const Tr = styled.tr``;
+export const U = styled.u``;
+export const Video = styled.video``;
